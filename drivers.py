021a9288da175e7340729902e688c64d862dd263
Ryan Carroll <rjc73432@uga.edu>
2019-12-09 19:54:42 -0500

    added python to create driver api page

---
+++ drivers.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import json
+
+def application(env, start_response):
+    """the main uWSGI application for displaying data from the MySQL database on a webpage"""
+    start_response('200 OK',[('Content-Type', 'text/html')])
+
+    #access the SQL database with credentials of user 'foo'
+    creds = {'user':'foo',
+             'database':'finalproject',
+             'password':'Passwd123!',
+             'auth_plugin':'mysql_native_password'}
+    cnx = mysql.connector.connect(**creds)
+    cursor = cnx.cursor(dictionary=True)
+    
+    testquery = "SELECT * from drivers;"
+
+    cursor.execute(testquery)
+    testreturn = cursor.fetchall()
+
+    html = json.dumps(testreturn)
+    
+    return html.encode()
